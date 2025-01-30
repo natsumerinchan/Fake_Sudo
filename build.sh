b50982d100aa183cb6554fbf6858a389d41fd9bd
Celica Sylphil <kamikitakomaric@protonmail.com>
2025-01-30 09:16:37 +0800

    Update

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$1" = "clean" ];then
 fi
 
 (cd src && ndk-build)
-
+ARCHS=(arm arm64 x86 x86_64 riscv64)
 cp README.md magisk_module/
 cp LICENSE magisk_module/
 cp src/libs/armeabi-v7a/sudo magisk_module/bin/arm
@@ -15,4 +15,4 @@ cp src/libs/x86/sudo magisk_module/bin/x86
 cp src/libs/x86_64/sudo magisk_module/bin/x86_64
 cp src/libs/riscv64/sudo magisk_module/bin/riscv64
 
-(cd magisk_module && zip -r ../sudo-ndk.zip -x "*.DS_Store" -- *)
+(cd magisk_module && zip -r ../sudo-ndk.zip -x "*/\.*" -x "\.*" -- *)
